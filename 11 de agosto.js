fba844b34335dc3c424e9d8b4c92a8dbd1832024
samly <samlyguzman12@gmail.com>
2021-08-17 10:54:27 -0500

    Cree mis archivos donde subire lo aprendido en las clases

---
+++ 11 de agosto.js
@@ -0,0 +1,12 @@
+<
+
+
+var listadodedatos = [{nombre:"john", apellidos:"castblanco "}]
+
+var listadedatos = [];
+listadedatos.push(john)
+listadedatos.push(john)
+listadedatos.push(john)
+
+console.log(listadedatos)[2]
+
